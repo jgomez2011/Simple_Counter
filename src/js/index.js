08bc29be43472e06564fe0e0c490b3ae6a0c29e2
jgomez2011 <jesus.gomez0911@gmail.com>
2023-07-27 19:47:19 +0000

    Contador

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,46 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import propTypes from "prop-types";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+const Contador = (props) => {
+	return (
+		<div className="counter">
+			<div className="clock">
+				<i className="fas fa-clock"></i>
+			</div>
+            <div className="quinto">{props.digitoCinco % 10}</div>
+			<div className="cuarto">{props.digitoCuatro % 10}</div>
+			<div className="tercero">{props.digitoTres % 10}</div>
+			<div className="segundo">{props.digitoDos % 10}</div>
+			<div className="primero">{props.digitoUno % 10}</div>
+		</div>
+	);
+};
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+Contador.propTypes = {
+	digitoUno: propTypes.number,
+	digitoDos: propTypes.number,
+	digitoTres: propTypes.number,
+	digitoCuatro: propTypes.number,
+    digitoCinco: propTypes.number,
+};
+
+let counter = 0;
+setInterval(() => {
+    let cinco = Math.floor(counter / 10000)
+	let cuatro = Math.floor(counter / 1000);
+	let tres = Math.floor(counter / 100);
+	let dos = Math.floor(counter / 10);
+	let uno = Math.floor(counter / 1);
+	counter++;
+
+	//render your react application
+	ReactDOM.render(
+		<Contador digitoUno={uno} digitoDos={dos} digitoTres={tres} digitoCuatro={cuatro} digitoCinco={cinco} />,
+		document.querySelector("#app")
+	);
+}, 1000);
